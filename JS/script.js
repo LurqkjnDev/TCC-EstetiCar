64604a18720ca6be6627053f5373a3388ecbeea8
Victor Straioto <129329645+LurqkjnDev@users.noreply.github.com>
2024-08-16 17:13:18 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,26 @@
+function exibeMenu() {
+    menu.style.display = "block"
+    iconHam.style.display = "none"
+    iconClose.style.display = "block"
 
+}
+
+function fechaMenu() {
+    menu.style.display = "none"
+    iconClose.style.display = "none"
+    iconHam.style.display = "block"
+}
+
+function verificaTela() {
+    if (window.innerWidth < 1024) {
+        iconHamb.style.display = "block"
+        iconClose.style.display = "none"
+        menu.style.display = "none"
+
+    } else {
+        iconHam.style.display = "none"
+        iconClose.style.display = "none"
+        menu.style.display = "block"
+    }
+}
+window.addEventListener("resize", verificaTela)
